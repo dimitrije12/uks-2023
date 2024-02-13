57fbcc95c6cd1cd2a5bb304022a73b79584baf00
jovan12121 <101191754+jovan12121@users.noreply.github.com>
2024-02-13 16:06:39 +0100

    added login and register functions

---
+++ Backend/Controllers/UsersController.cs
@@ -0,0 +1,66 @@
+﻿using Backend.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUsersService _usersService;
+        public UsersController(IUsersService usersService)
+        {
+            _usersService = usersService;
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            try
+            {
+                return Ok(await _usersService.LoginAsync(username, password));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string username, string password, string email)
+        {
+            try
+            {
+                return Ok(await _usersService.RegisterAsync(username, password, email));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        [HttpPut("update")]
+        public async Task<IActionResult> Update(string username, string email)
+        {
+            try
+            {
+                return Ok(await _usersService.UpdateAsync(username, email));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        [HttpPut("changepassword")]
+        public async Task<IActionResult> Update(string username, string oldPassword, string newPassword)
+        {
+            try
+            {
+                return Ok(await _usersService.ChangePasswordAsync(username,oldPassword,newPassword));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
